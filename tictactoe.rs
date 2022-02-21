663b7c55a834ce3ad30699628ea1b16649536115
linhta71 <linhta71@email.arizona.edu>
2022-02-21 01:05:42 -0700

    Changed the struct a bit

--- tictactoe.rs
+++ tictactoe.rs
@@ -2,22 +2,47 @@
 
 struct Board {
     board: [u8; 9],
+    cur_player: u8,
+    prev_player: u8,
+    moves: u8,
 }
 
 impl Board {
     fn new() -> Board {
         Board {
             board: [0;9],
+            cur_player: 1u8,
+            prev_player: 2u8,
+            moves: 0u8,
         }  
     }
 
-    fn put(&mut self, index: usize, player:u8) -> bool {
+    fn change_player(&mut self) {
+        if self.cur_player == 1 {
+            self.cur_player = 2;
+            self.prev_player = 1
+        } else {
+            self.cur_player = 1;
+            self.prev_player = 2;
+        }
+    }
+
+    fn put(&mut self, index: usize) {
         if self.board[index] != 0 {
             println!("This is already occupied");
-            return false;
+        } else {
+            self.board[index] = self.cur_player;
+            self.change_player();
+            self.moves+=1;
         }
-        self.board[index] = player;
-        return true;
+    }
+
+    fn get_current_player(&mut self) -> char {
+        match self.cur_player {
+            1 => 'X',
+            2 => 'O',
+            _ => ' ',
+        }
     }
 
     fn display(&self) {
@@ -45,8 +70,9 @@ fn main() {
     let mut board: Board = Board::new();
     board.display();
     let index = 1 as usize;
-    board.put(index, 1);
+    board.put(index);
     board.display();
-    board.put(2 as usize, 2);
+    board.put(2 as usize);
     board.display();
+    println!("Current {}", board.get_current_player());
 }
