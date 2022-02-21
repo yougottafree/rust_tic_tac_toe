8e771893aa85b594759cdbf0cf1cd128e83323a7
linhta71 <linhta71@email.arizona.edu>
2022-02-20 22:43:04 -0700

    added function to print

--- tictactoe.rs
+++ tictactoe.rs
@@ -1,13 +1,41 @@
+// use std::fmt;
 
-enum Player {
-    X,
-    Y,
+struct Board {
+    board: [u8; 9],
+    player: u8,
 }
 
-struct Board<'a> {
-    board: &'a [Player],
+impl Board {
+    fn new() -> Board {
+        let mut newBoard = Board {
+            board: [0;9], 
+            player: 1,
+        };
+        return newBoard;
+    }
+
+    fn display(&self) {
+        println!("__________________");
+        println!("|     |     |     |");
+        for i in 0..9 {
+            print!("|  ");
+            match self.board[i] {
+                1 => print!("X"),
+                2 => print!("O"),
+                _ => print!("{}", i),
+            }
+            match i {
+                2 => print!("  |\n|_____|_____|_____|\n|     |     |     |\n"),
+                5 => print!("  |\n|_____|_____|_____|\n|     |     |     |\n"),
+                8 => print!("  |\n|_____|_____|_____|\n"),
+                _ => print!("  "),
+            }
+        }
+    }
+
 }
 
 fn main() {
-    println!("Hello");
+    let board: Board = Board::new();
+    board.display();
 }
